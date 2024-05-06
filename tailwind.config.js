684498ba3d0152d32bfa5b76be73d27e0b894290
Oleg Nurzhanov <oleg@nastyle.pro>
2024-05-06 14:05:11 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {
-            gridTemplateRows: {
-               'layout': 'auto 1fr auto',
-            },
-          },
+        extend: {},
     },
     plugins: [],
 }
